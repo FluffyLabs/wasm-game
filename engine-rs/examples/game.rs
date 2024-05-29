a17b9c694584ca1dd1ac79f5bbd1574d85231b0e
Tomasz Drwięga <tomusdrw@gmail.com>
2024-05-29 13:11:46 +0200

    Fix collisions and bouncing.

--- engine-rs/examples/game.rs
+++ engine-rs/examples/game.rs
@@ -1,17 +1,129 @@
+use engine_rs::board::State;
+use sdl2::video::Window;
+use sdl2::{pixels::Color, render::Canvas};
+use sdl2::rect::{Point, Rect};
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time;
+
 use engine_rs::{board::Board, game::{game_loop, Game, Position}};
 
+fn main() -> Result<(), String> {
+    // sdl init
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let size = 640;
 
-fn main() {
+    let window = video_subsystem
+        .window("Arkanoid Example", size, size)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    // game init
     let board = Board::new(16);
-    let viewport_size = Position { x: 160.0, y: 160.0 };
-    let start_time = std::time::Instant::now();
+    let viewport_size = Position { x: size as f32, y: size as f32 };
+    let start_time = time::Instant::now();
     let mut game = Game::new(board, 0, viewport_size);
 
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(10));
-        let new_time = std::time::Instant::now().duration_since(start_time).as_millis();
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        std::thread::sleep(time::Duration::from_millis(50));
+        // process events
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'running;
+                    },
+                _ => {}
+            }
+        }
+
+        // process the game
+        let new_time = time::Instant::now().duration_since(start_time).as_millis();
         game_loop(&mut game, new_time as u64);
 
-        println!("{:?}", game);
+        render(&mut canvas, &game)?;
     }
+
+    Ok(())
+}
+
+fn render(
+    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+    game: &Game
+) -> Result<(), String> {
+    canvas.clear();
+
+    let lit_color = Color::RGB(80, 250, 60);
+    let dark_color = Color::RGB(175, 238, 238);
+
+    let board = game.board();
+    let board_size = board.size();
+    let cell_size = game.cell_size();
+    for row in 0..board_size {
+        for col in 0..board_size {
+            let kind = board.cell(col, row);
+            canvas.set_draw_color(match kind {
+                State::Dark => Color::RGB(152, 251, 152),
+                State::Lit => Color::RGB(135, 206, 250),
+            });
+            canvas.fill_rect(Rect::new(
+                row as i32 * cell_size.x as i32,
+                col as i32 * cell_size.y as i32,
+                cell_size.x as u32,
+                cell_size.y as u32,
+            ))?;
+        }
+    }
+
+    let ball_radius = ((cell_size.x + cell_size.y) / 4.0) as i32;
+
+    canvas.set_draw_color(dark_color);
+    let b = game.lit_ball();
+    draw_filled_circle(canvas, b.x as i32 , b.y as i32, ball_radius)?;
+
+    canvas.set_draw_color(lit_color);
+    let b = game.dark_ball();
+    draw_filled_circle(canvas, b.x as i32 , b.y as i32, ball_radius)?;
+
+    canvas.present();
+    Ok(())
+}
+
+
+fn draw_filled_circle(canvas: &mut Canvas<Window>, x0: i32, y0: i32, radius: i32) -> Result<(), String> {
+    let mut x = radius;
+    let mut y = 0;
+    let mut radius_error = 1 - x;
+
+    while x >= y {
+        draw_line(canvas, x0 - x, y0 + y, x0 + x, y0 + y)?;
+        draw_line(canvas, x0 - y, y0 + x, x0 + y, y0 + x)?;
+        draw_line(canvas, x0 - x, y0 - y, x0 + x, y0 - y)?;
+        draw_line(canvas, x0 - y, y0 - x, x0 + y, y0 - x)?;
+
+        y += 1;
+        if radius_error < 0 {
+            radius_error += 2 * y + 1;
+        } else {
+            x -= 1;
+            radius_error += 2 * (y - x + 1);
+        }
+    }
+
+    Ok(())
+}
+
+fn draw_line(canvas: &mut Canvas<Window>, x1: i32, y1: i32, x2: i32, y2: i32) -> Result<(), String> {
+    canvas.draw_line(Point::new(x1, y1), Point::new(x2, y2))?;
+    Ok(())
 }
--- engine-rs/src/game.rs
+++ engine-rs/src/game.rs
@@ -17,32 +17,34 @@ struct Movement {
 
 impl Movement {
     fn apply(&self, time_diff: f32, position: &mut Position) {
-        let position_diff = self.speed as f32 / 64.0 * time_diff / 4.0;
+        let position_diff = self.speed as f32 / 64.0 * time_diff / 40.0;
 
         let angle = self.angle as f32;
-        let mut y_component = (angle % 90.0) / 90.0;
-        let mut x_component = 1.0 - y_component;
+        let a_component = (self.angle % 90) as f32 / 90.0;
+        let b_component = 1.0 - a_component;
 
         let quadrant = angle / 90.0;
-        if quadrant >= 1.0 && quadrant < 2.0 {
-            x_component = -x_component;
-        }
-        if quadrant >= 2.0 && quadrant < 3.0 {
-            x_component = -x_component;
-            y_component = -y_component;
-        }
-        if quadrant >= 3.0 {
-            y_component = -y_component;
-        }
-
+        let (x_component, y_component) = match quadrant {
+            q if q < 1.0 => (b_component, a_component),
+            q if q >= 1.0 && q < 2.0 => (-a_component, b_component),
+            q if q >= 2.0 && q < 3.0 => (-a_component, -b_component),
+            _ => (a_component, -b_component),
+        };
+        
+        println!("Angle: {}, x_component: {}, y_component: {}", angle, x_component, y_component);
         position.x += position_diff * x_component;
         position.y += position_diff * y_component;
     }
 
-    fn bounce(&mut self) {
-        // TODO [ToDr] angle variations.
-        self.angle = (self.angle + 180) % 360;
-        // self.speed += 1;
+    fn bounce(&mut self, collision_type: CollisionType) {
+        let before_angle = self.angle;
+        let speed_factor = self.speed as u16 * 3;
+        self.angle = match collision_type {
+            CollisionType::Horizontal => (540 - self.angle + speed_factor) % 360,
+            CollisionType::Vertical => (360 - self.angle + speed_factor) % 360,
+        };
+
+        println!("Bounce! {} -> {}", before_angle, self.angle);
     }
 }
 
@@ -52,11 +54,29 @@ pub struct Game {
     viewport_size: Position,
     time: Timestamp,
     cell_size: Position,
+    ball_radius: Coordinate,
     lit_ball: (Position, Movement),
     dark_ball: (Position, Movement),
 }
 
 impl Game {
+
+    pub fn board(&self) -> &Board {
+        &self.board
+    }
+
+    pub fn lit_ball(&self) -> &Position {
+        &self.lit_ball.0
+    }
+
+    pub fn dark_ball(&self) -> &Position {
+        &self.dark_ball.0
+    }
+
+    pub fn cell_size(&self) -> &Position {
+        &self.cell_size
+    }
+
     pub fn new(
         board: Board,
         start_time: Timestamp,
@@ -72,12 +92,12 @@ impl Game {
         let init_pos_lit = Position { x: half_view.x / 2.0, y};
 
         let movement_dark = Movement {
-            angle: 180,
+            angle: 225,
             speed: 1,
         };
 
         let movement_lit = Movement {
-            angle: 0,
+            angle: 45,
             speed: 1,
         };
 
@@ -89,11 +109,14 @@ impl Game {
         assert!(cell_size.x > 1.0, "The viewport size is too small to draw a cell");
         assert!(cell_size.y > 1.0, "The viewport size is too small to draw a cell");
 
+        let ball_radius = (cell_size.x + cell_size.y) / 4.0;
+
         Self {
             board,
             time: start_time,
             viewport_size,
             cell_size,
+            ball_radius,
             lit_ball: (init_pos_lit, movement_lit),
             dark_ball: (init_pos_dark, movement_dark),
         }
@@ -115,11 +138,11 @@ pub fn game_loop(game: &mut Game, time: Timestamp) {
         // 2. check collisions:
         //  2.2. With boundaries
         //      2.2.1 bounce balls
-        Collisions::boundaries(position, movement, &game.viewport_size);
+        Collisions::boundaries(position, movement, game.ball_radius, &game.viewport_size);
         //  2.1. With board items: 
         //      2.1.1. flip board elements
         //      2.1.2. bounce balls
-        Collisions::board(position, movement, &game.cell_size, &mut game.board, kind);
+        Collisions::board(position, movement, game.ball_radius, &game.cell_size, &mut game.board, kind);
     }
 }
 
@@ -129,50 +152,124 @@ impl Collisions {
     fn boundaries(
         position: &mut Position,
         movement: &mut Movement,
+        ball_radius: Coordinate,
         viewport_size: &Position,
     ) {
-        let mut should_bounce = false;
+        let mut collision_type = None;
         // check collisions with the environment.
-        if position.x < 0.0 {
-            position.x = 0.0;
-            should_bounce = true;
+        if position.x < ball_radius {
+            position.x = ball_radius;
+            collision_type = Some(CollisionType::Horizontal);
         }
-        if position.x >= viewport_size.x {
-            position.x = viewport_size.x - 1.0;
-            should_bounce = true;
+        if position.x >= viewport_size.x - ball_radius {
+            position.x = viewport_size.x - ball_radius - 1.0;
+            collision_type = Some(CollisionType::Horizontal);
         }
-        if position.y < 0.0 {
-            position.y = 0.0;
-            should_bounce = true;
+        if position.y < ball_radius {
+            position.y = ball_radius;
+            collision_type = Some(CollisionType::Vertical);
         }
-        if position.y >= viewport_size.y {
-            position.y = viewport_size.y - 1.0;
-            should_bounce = true;
+        if position.y >= viewport_size.y - ball_radius {
+            position.y = viewport_size.y - ball_radius - 1.0;
+            collision_type = Some(CollisionType::Vertical);
         }
 
         // bounce only once!
-        if should_bounce {
-            movement.bounce();
+        if let Some(collision_type) = collision_type {
+            movement.bounce(collision_type);
         }
     }
 
     fn board(
         position: &mut Position,
         movement: &mut Movement,
+        ball_radius: Coordinate,
         cell_size: &Position,
         board: &mut Board,
         kind: board::State,
     ) {
-        let cell_x = (position.x / cell_size.x).floor() as board::Index;
-        let cell_y = (position.y / cell_size.y).floor() as board::Index;
-    
-        let at_kind = board.cell(cell_y, cell_x);
-        if kind != at_kind {
-            // flip the cell
-            board.flip(cell_y, cell_x);
-            // now bounce the ball depending on the approach angle.
-            // TODO [ToDr] calculate angle
-            movement.bounce();
+        let mut collision_type = None;
+        for box_x in [position.x + ball_radius, position.x - ball_radius] {
+            for box_y in [position.y + ball_radius, position.y - ball_radius] {
+                let cell_x = (box_x / cell_size.x).floor() as board::Index;
+                let cell_y = (box_y / cell_size.y).floor() as board::Index;
+
+                let at_kind = board.cell(cell_y, cell_x);
+                if kind != at_kind {
+                    // flip the cell
+                    board.flip(cell_y, cell_x);
+                    let cell_pos_x  = cell_x as f32 * cell_size.x;
+                    let cell_pos_y = cell_y as f32 * cell_size.y;
+                    collision_type = if (cell_pos_x - position.x).abs() < (cell_pos_y - position.y).abs() {
+                        Some(CollisionType::Horizontal)
+                    } else {
+                        Some(CollisionType::Vertical)
+                    };
+                }
+            }
+        }
+        if let Some(collision_type) = collision_type {
+            movement.bounce(collision_type);
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum CollisionType {
+    Horizontal,
+    Vertical,
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_calculate_horizontal_bounce_angle_correctly() {
+        let values = vec![
+            (0, 180),
+            (15, 165),
+            (30, 150),
+            (65, 115),
+            (89, 91),
+            (90, 90), //edge case?
+            (270, 270), //edge case?
+            (105, 75),
+        ];
+
+        for (angle, expected) in values {
+            let mut mov = Movement {
+                angle,
+                speed: 0,
+            };
+            // when
+            mov.bounce(CollisionType::Horizontal);
+
+            // then
+            assert_eq!(mov.angle, expected);
+        }
+    }
+
+    #[test]
+    fn should_calculate_vertical_bounce_angle_correctly() {
+        let values = vec![
+            (90, 270),
+            (0, 0), //edge case?
+            (180, 180), //edge case?
+        ];
+
+        for (angle, expected) in values {
+            let mut mov = Movement {
+                angle,
+                speed: 0,
+            };
+            // when
+            mov.bounce(CollisionType::Vertical);
+
+            // then
+            assert_eq!(mov.angle, expected);
+        }
+    }
+
 }
